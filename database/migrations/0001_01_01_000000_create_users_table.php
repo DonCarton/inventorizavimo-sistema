28230b9bcbbc4f5bb79aebd094085851909d3299
DonCarton <50633087+DonCarton@users.noreply.github.com>
2024-05-24 10:40:37 +0300

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->foreignId('laboratory')->nullable()->constrained('laboratories');
             $table->foreignId('created_by')->constrained('users');
             $table->foreignId('updated_by')->constrained('users');
         });
