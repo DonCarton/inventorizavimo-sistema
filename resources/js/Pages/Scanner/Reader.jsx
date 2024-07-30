537489fc1ec00926efa4b78a1a299a28cfd6500a
CygasD <cygas.dominykas@gmail.com>
2024-07-30 19:26:31 +0200

    Camera testing

--- resources/js/Pages/Scanner/Reader.jsx
+++ resources/js/Pages/Scanner/Reader.jsx
@@ -1,12 +1,13 @@
 import {Html5Qrcode, Html5QrcodeSupportedFormats} from 'html5-qrcode';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.jsx';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {Head, router} from "@inertiajs/react";
 import {__} from "@/Libs/Lang.jsx";
 import FailureMessage from "@/Components/FailureMessage.jsx";
 import SuccessMessage from "@/Components/SuccessMessage.jsx";
 
 export default function Reader({auth, role, success, failure}) {
+    const [emptyValue, setEmptyValue] = useState(null);
     useEffect(() => {
         const scanner = new Html5Qrcode(
             "reader", {
@@ -18,7 +19,19 @@ export default function Reader({auth, role, success, failure}) {
         }
         const config = {fps: 10, qrbox: {width: 250, height: 250}};
 
-        scanner.start({facingMode: "environment"}, config, qrCodeSuccessCallback);
+        scanner.start({facingMode: "environment", deviceId: 2}, config, qrCodeSuccessCallback);
+        Html5Qrcode.getCameras().then((devices) => {
+            if (devices && devices.length) {
+                // Find the back camera
+                const backCamera = devices.find(device => device.label.toLowerCase().includes('back') || device.label.toLowerCase().includes('environment'));
+                console.log(devices);
+                setEmptyValue(devices);
+                const cameraId = backCamera ? backCamera.id : devices[0].id;
+                startScanner(cameraId);
+            }
+        }).catch(err => {
+            console.error(err);
+        });
     }, []);
     return (
         <AuthenticatedLayout
@@ -32,6 +45,7 @@ export default function Reader({auth, role, success, failure}) {
                 <div className="max-w-2xl mx-auto sm:px-6 lg:px-8">
                     {success && (<SuccessMessage message={success}/>)}
                     {failure && (<FailureMessage message={failure}/>)}
+                    {emptyValue !== null && <h1>{emptyValue[0]}{emptyValue[1]}</h1>}
                     <div id='reader'/>
                 </div>
             </div>
