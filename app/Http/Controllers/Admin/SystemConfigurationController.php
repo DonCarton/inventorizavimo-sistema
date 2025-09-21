9d992524391aa79d0e8be9b4de0cb819d4f4f63f
Dominykas Cygas <cygas.dominykas@gmail.com>
2025-09-21 16:37:06 +0200

    Validate file inputs on the front and back-end. Permit the system configuration page on PRODUCTION :)

--- app/Http/Controllers/Admin/SystemConfigurationController.php
+++ app/Http/Controllers/Admin/SystemConfigurationController.php
@@ -17,69 +17,99 @@ class SystemConfigurationController extends Controller
 {
     public function index(): Response|RedirectResponse
     {
-        if (strcasecmp(config('app.env'), string2: 'local') != 0) {
-            return redirect()->back();
-        }
-
-        $systemConfiguration = SystemConfiguration::all()->groupBy('category');
-        $myConfigurations = SystemConfiguration::with('value')->get()->groupBy('category');
-        return Inertia::render('Admin/Index', [
-            'systemConfiguration' => $systemConfiguration,
-            'myConfigurations' => $myConfigurations,
-            'success' => session('success'),
-            'failure' => session('failure')
+        $systemConfiguration = SystemConfiguration::all()->groupBy("category");
+        $myConfigurations = SystemConfiguration::with("value")
+            ->get()
+            ->groupBy("category");
+        return Inertia::render("Admin/Index", [
+            "systemConfiguration" => $systemConfiguration,
+            "myConfigurations" => $myConfigurations,
+            "success" => session("success"),
+            "failure" => session("failure"),
         ]);
     }
     public function edit(): Response|RedirectResponse
     {
-        if (strcasecmp(config('app.env'), string2: 'local') != 0) {
+        if (strcasecmp(config("app.env"), string2: "local") != 0) {
             return redirect()->back();
         }
 
-        $systemConfiguration = SystemConfiguration::all()->groupBy('category');
-        $myConfigurations = SystemConfiguration::with('value')->get()->groupBy('category');
-        return Inertia::render('Admin/Edit', [
-            'systemConfiguration' => $systemConfiguration,
-            'myConfigurations' => $myConfigurations,
-            'success' => session('success'),
-            'failure' => session('failure')
+        $systemConfiguration = SystemConfiguration::all()->groupBy("category");
+        $myConfigurations = SystemConfiguration::with("value")
+            ->get()
+            ->groupBy("category");
+        return Inertia::render("Admin/Edit", [
+            "systemConfiguration" => $systemConfiguration,
+            "myConfigurations" => $myConfigurations,
+            "success" => session("success"),
+            "failure" => session("failure"),
         ]);
     }
 
-    public function update(SystemConfiguration $systemConfiguration, Request $request): JsonResponse|RedirectResponse
-    {
-        $configurationValue = ConfigurationValue::findOrFail($systemConfiguration->id);
+    public function update(
+        SystemConfiguration $systemConfiguration,
+        Request $request,
+    ): JsonResponse|RedirectResponse {
+        $configurationValue = ConfigurationValue::findOrFail(
+            $systemConfiguration->id,
+        );
 
         $validated = $request->validate([
-            'value' => ['required', 'string', function ($attribute, $value, $fail) use ($configurationValue) {
-                if (str_contains($configurationValue->configuration->key, "_range")){
-                    if (!$this->isValidRange($value, $configurationValue->configuration->key)) {
-                        if ($configurationValue->configuration->key === 'cupboard_range') {
-                            $fail(__('validation.custom.value.range_alphabetical'));
-                        } elseif ($configurationValue->configuration->key === 'shelf_range') {
-                            $fail(__('validation.custom.value.range_numeric'));
+            "value" => [
+                "required",
+                "string",
+                function ($attribute, $value, $fail) use ($configurationValue) {
+                    if (
+                        str_contains(
+                            $configurationValue->configuration->key,
+                            "_range",
+                        )
+                    ) {
+                        if (
+                            !$this->isValidRange(
+                                $value,
+                                $configurationValue->configuration->key,
+                            )
+                        ) {
+                            if (
+                                $configurationValue->configuration->key ===
+                                "cupboard_range"
+                            ) {
+                                $fail(
+                                    __(
+                                        "validation.custom.value.range_alphabetical",
+                                    ),
+                                );
+                            } elseif (
+                                $configurationValue->configuration->key ===
+                                "shelf_range"
+                            ) {
+                                $fail(
+                                    __("validation.custom.value.range_numeric"),
+                                );
+                            }
+                            $fail(__("validation.custom.value.range_generic"));
                         }
-                        $fail(__('validation.custom.value.range_generic'));
                     }
-                }
-            }],
+                },
+            ],
         ]);
 
         $configurationValue->update($validated);
-        return to_route('systemConfigurations.index');
+        return to_route("systemConfigurations.index");
     }
     private function isValidRange($range, $key): bool
     {
-        $rangeParts = explode('-', $range);
+        $rangeParts = explode("-", $range);
         if (count($rangeParts) !== 2) {
             return false;
         }
 
         [$start, $end] = $rangeParts;
 
-        if ($key === 'cupboard_range') {
-            return $start === 'A' && ctype_alpha($end) && $start <= $end;
-        } elseif ($key === 'shelf_range') {
+        if ($key === "cupboard_range") {
+            return $start === "A" && ctype_alpha($end) && $start <= $end;
+        } elseif ($key === "shelf_range") {
             return $start == 1 && ctype_digit($end) && $start <= $end;
         }
 
